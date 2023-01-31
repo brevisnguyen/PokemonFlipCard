be1d8f5bef9776f939f3374674f9fe6fb9b3d222
brevis-ng <brevisnguyen@gmail.com>
2023-01-31 10:57:56 +0800

    1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,6 +4,7 @@
 
 <template>
 	<div class="py-3 mx-auto bottom-0 w-full fixed">
-		<p class="text-gray-50 text-center text-lg sm:text-sm">This game is created by Brevis Nguyen while learning Vue 3 - <a href="https://github.com/brevis-ng/PokemonFlipCard" class="text-amber-400">view hear</a></p>
+		<p class="text-gray-50 text-center text-lg sm:text-sm">This game is created by Brevis Nguyen while learning Vue
+			3 - <a href="#" class="text-amber-400">view hear</a></p>
 	</div>
 </template>
